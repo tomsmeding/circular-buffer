fc27ce036e319eebbe8051b5ddaef8ebd0bf5c4e
tomsmeding <hallo@tomsmeding.nl>
2015-04-21 08:51:21 +0200

    Switch to a object prototype

--- index.js
+++ index.js
@@ -3,43 +3,51 @@ function CircularBuffer(capacity){
 	if(typeof capacity!="number"||capacity%1!=0||capacity<1)
 		throw new TypeError("Invalid capacity");
 
-	var buffer=new Array(capacity),first=capacity,size=0;
-	this.size=function(){return size;};
-	this.capacity=function(){return capacity;};
-	this.enq=function(value){
-		if(first>0)first--; else first=capacity-1;
-		buffer[first]=value;
-		if(size<capacity)size++;
-	};
-	this.deq=function(){
-		if(size==0)throw new RangeError("CircularBuffer dequeue on empty buffer");
-		var value=buffer[first];
-		if(first==capacity-1)first=0; else first++;
-		size--;
-		return value;
-	};
-	this.get=function(start,end){
-		if(size==0&&start==0&&(end==undefined||end==0))return [];
-		if(typeof start!="number"||start%1!=0||start<0)throw new TypeError("Invalid start");
-		if(start>=size)throw new RangeError("Index past end of buffer: "+start);
+	this._buffer=new Array(capacity);
+	this._capacity=capacity;
+	this._first=capacity;
+	this._size=0;
+}
 
-		if(end==undefined)return buffer[(first+start)%capacity];
+CircularBuffer.prototype={
+	size:function(){return this._size;},
+	capacity:function(){return this._capacity;},
+	enq:function(value){
+		if(this._first>0)this._first--; else this._first=this._capacity-1;
+		this._buffer[this._first]=value;
+		if(this._size<this._capacity)this._size++;
+	},
+	deq:function(){
+		if(this._size==0)throw new RangeError("CircularBuffer dequeue on empty buffer");
+		var value=this._buffer[this._first];
+		if(this._first==this._capacity-1)this._first=0; else this._first++;
+		this._size--;
+		return value;
+	},
+	get:function(start,end){
+		if(this._size==0&&start==0&&(end==undefined||end==0))return [];
+		if(typeof start!="number"||start%1!=0||start<0)throw new TypeError("Invalid start");
+		if(start>=this._size)throw new RangeError("Index past end of buffer: "+start);
+
+		if(end==undefined)return this._buffer[(this._first+start)%this._capacity];
 
 		if(typeof end!="number"||end%1!=0||end<0)throw new TypeError("Invalid end");
-		if(end>=size)throw new RangeError("Index past end of buffer: "+end);
+		if(end>=this._size)throw new RangeError("Index past end of buffer: "+end);
 
-		if(first+start>=capacity){
+		if(this._first+start>=this._capacity){
 			//make sure first+start and first+end are in a normal range
-			start-=capacity; //becomes a negative number
-			end-=capacity;
+			start-=this._capacity; //becomes a negative number
+			end-=this._capacity;
 		}
-		if(first+end<capacity)return buffer.slice(first+start,first+end+1);
-		else return buffer.slice(first+start,capacity).concat(buffer.slice(0,first+end+1-capacity));
-	};
-	this.toarray=function(){
-		if(size==0)return [];
-		return this.get(0,size-1);
+		if(this._first+end<this._capacity)
+			return this._buffer.slice(this._first+start,this._first+end+1);
+		else
+			return this._buffer.slice(this._first+start,this._capacity).concat(this._buffer.slice(0,this._first+end+1-this._capacity));
+	},
+	toarray:function(){
+		if(this._size==0)return [];
+		return this.get(0,this._size-1);
 	}
-}
+};
 
 module.exports=CircularBuffer;
